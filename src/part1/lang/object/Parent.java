cf7978763ec6037eabc1b5c5870bdb87265137e0
Min <als904204@naver.com>
2024-05-22 16:04:19 +0900

    Object 클래스는 모든 클래스의 부모

---
+++ src/part1/lang/object/Parent.java
@@ -0,0 +1,8 @@
+package part1.lang.object;
+
+// 상속받는 클래스가 없다면, 묵시적으로 Object 를 상속 받는다.
+public class Parent {
+    public void parentMethod(){
+        System.out.println("Parent.parentMethod");
+    }
+}
