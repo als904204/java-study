1b00ce557c9d24c553106cc388baf3b18ef8e451
Min <als904204@naver.com>
2024-06-09 15:54:15 +0900

    6. 불변객체가 아닐 때 값을 수정한다

---
+++ src/part1/lang/object/immutable/change/MutableMain.java
@@ -0,0 +1,14 @@
+package part1.lang.object.immutable.change;
+
+public class MutableMain {
+
+    public static void main(String[] args) {
+        MutableObj obj = new MutableObj(10);
+        obj.add(20);
+
+        // 계산 이후의 기존값(10)은 삭제가 되고 30이 되어 버림
+        System.out.println("obj.getValue() = " + obj.getValue());
+
+    }
+
+}
